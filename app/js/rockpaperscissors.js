62bb1cc7b9683250f9e0eadd41bbb2c433aa2f79
rotatetranslate <adamberro@gmail.com>
2016-04-13 14:14:27 -0700

    finalized & tested RPS game

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -20,6 +20,7 @@ function randomPlay() {
 ////////////////////////////////////////////////
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
+var playerMove = null;
 
 function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
@@ -33,6 +34,8 @@ function getPlayerMove(move) {
     }
 }
 
+var computerMove = null;
+
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
@@ -45,8 +48,12 @@ function getComputerMove(move) {
     }
 }
 
-function getWinner(playerMove,computerMove) {
-    var winner;
+var winner = null;
+
+function getWinner(playerMove, computerMove) {
+    //var winner;
+    //var playerMove = getPlayerMove();
+    //var computerMove = getComputerMove();
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
@@ -58,7 +65,6 @@ function getWinner(playerMove,computerMove) {
     } else {
         winner = 'computer';
     }
-
     return winner;
 }
 
@@ -68,6 +74,29 @@ function playToFive() {
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+    while (playerWins < 5 && computerWins < 5){
+        playerMove = getPlayerMove();
+        computerMove = getComputerMove();
+        winner = getWinner(playerMove, computerMove);
+
+        switch (winner){
+            case 'player': console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+            playerWins++;
+            break;
+            case 'computer': console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+            computerWins++;
+            break;
+            case 'tie': console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        }
+        console.log('Current score: player - ' + playerWins + ' | computer - ' + computerWins);
+    }
+
+    if (playerWins === 5){
+        alert("player wins!");
+    } else {
+        alert("computer wins!");
+    }
 }
+
+playToFive();
 
